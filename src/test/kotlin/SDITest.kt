06937e1337fadfe7a4ceece327d4575a584d4eec
Luismi Barcos <luismiguel.barcos@liferay.com>
2023-02-18 20:47:04 +0100

    Add first simple test

---
+++ src/test/kotlin/SDITest.kt
@@ -0,0 +1,29 @@
+import com.useraccount.services.UserAccountClient
+import com.useraccount.services.impl.UserServiceImpl
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.sdi.injector.Injector
+
+/**
+ *@author Luis Miguel Barcos
+ */
+class SDITest {
+
+    companion object {
+        private lateinit var injector: Injector
+        private lateinit var userAccountClient: UserAccountClient
+    }
+
+    @BeforeEach
+    fun setup() {
+        injector = Injector()
+        injector.initSDI(UserAccountClient::class.java.`package`.name)
+        userAccountClient = injector.getService(UserAccountClient::class.java) as UserAccountClient
+    }
+
+    @Test
+    fun test() {
+        val service = injector.getService(UserServiceImpl::class.java) as UserServiceImpl
+        assert(service.getUserName() == "username")
+    }
+}
