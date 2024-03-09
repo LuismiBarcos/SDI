35b2c287303df9096486c6ded848a745273de73c
Luismi Barcos <akiluismi@gmail.com>
2024-03-09 19:19:25 +0100

    Add domain classes to represent the pending injections

---
+++ src/main/kotlin/org/sdi/domain/Field.kt
@@ -0,0 +1,10 @@
+package org.sdi.domain
+
+import java.lang.reflect.Field
+
+/**
+ * @author Luis Miguel Barcos
+ */
+data class Field(
+    val value: Field
+)
---
+++ src/main/kotlin/org/sdi/domain/PendingInjection.kt
@@ -0,0 +1,9 @@
+package org.sdi.domain
+
+/**
+ * @author Luis Miguel Barcos
+ */
+class PendingInjection(
+    val instance: Instance,
+    val field: Field
+)
