7077615b81fdc1b5ce7576bf8a5e65da10dcc6f3
Luismi Barcos <akiluismi@gmail.com>
2024-03-23 16:37:11 +0100

    Create Component and Components domain class

---
+++ src/main/kotlin/org/sdi/domain/model/Component.kt
@@ -0,0 +1,9 @@
+package org.sdi.domain.model
+
+/**
+ * @author Luis Miguel Barcos
+ */
+data class Component(
+    val clazz: Clazz,
+    val implementation: Implementation
+)
---
+++ src/main/kotlin/org/sdi/domain/model/Components.kt
@@ -0,0 +1,8 @@
+package org.sdi.domain.model
+
+/**
+ * @author Luis Miguel Barcos
+ */
+data class Components(
+    val values: List<Component>
+)
