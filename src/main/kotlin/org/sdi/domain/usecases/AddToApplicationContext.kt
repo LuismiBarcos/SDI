5da9881f66516edc3c6499b2617b646d4f0030b5
Luismi Barcos <akiluismi@gmail.com>
2024-03-28 23:15:09 +0100

    Add AddToApplicationContext use case to handle every class that needs to be added to the application context. This will happen only if the class provided is a component, which means is annotated with the component annotation

---
+++ src/main/kotlin/org/sdi/domain/usecases/AddToApplicationContext.kt
@@ -0,0 +1,28 @@
+package org.sdi.domain.usecases
+
+import org.sdi.annotations.Component
+import org.sdi.domain.model.Clazz
+import org.sdi.domain.model.Instance
+import org.sdi.domain.usecases.helpers.AnnotationsHelper
+import org.sdi.domain.usecases.helpers.ComponentHandler
+
+/**
+ * @author Luis Miguel Barcos
+ */
+class AddToApplicationContext(
+    private val annotationsHelper: AnnotationsHelper,
+    private val componentHandler: ComponentHandler
+) {
+
+    /**
+     * Adds the class to the application context in case the class is annotated with the [Component] annotation
+     * @param clazz
+     */
+    fun add(clazz: Clazz) {
+        if(annotationsHelper.isComponent(clazz)) {
+            val componentInstance = Instance(clazz.value.getDeclaredConstructor().newInstance())
+            componentHandler.handleClasses(clazz, componentInstance)
+            componentHandler.handleFields(clazz, componentInstance)
+        }
+    }
+}
